df9e75764b7c26c857f6457b712d60c1fe93cbf6
PelerYuan <pelerlittlepig@gmail.com>
2025-07-13 20:34:56 +0800

    change model

--- server.py
+++ server.py
@@ -8,18 +8,6 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-def convert_ndarray(obj):
-    if isinstance(obj, dict):
-        return {k: convert_ndarray(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        return [convert_ndarray(v) for v in obj]
-    elif isinstance(obj, (np.ndarray, np.generic)):
-        return obj.tolist() if isinstance(obj, np.ndarray) else obj.item()
-    elif hasattr(obj, 'item'):
-        return obj.item()
-    else:
-        return obj
-
 @app.route('/analyze', methods=['POST'])
 def analyze_image():
     if 'image' not in request.files:
@@ -36,12 +24,12 @@ def analyze_image():
         raw_result = DeepFace.analyze(
             img_path=filepath,
             actions=['emotion'],
-            enforce_detection=True,
-            detector_backend='mtcnn'  # 使用更精准的人脸检测器
+            enforce_detection=False,
+            align=True,
+            detector_backend='mtcnn',  # 使用更精准的人脸检测器
         )
-        result = convert_ndarray(raw_result)
         return app.response_class(
-            response=json.dumps(result, ensure_ascii=False, indent=2, default=str),
+            response=json.dumps(raw_result, ensure_ascii=False, indent=2, default=str),
             status=200,
             mimetype='application/json'
         )
